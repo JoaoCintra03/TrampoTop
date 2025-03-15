97928610cb43215c69426093de9e832effb427c5
JoaoCintra03 <161538925+JoaoCintra03@users.noreply.github.com>
2025-03-14 22:28:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 let express = require('express')
+const db = require('./db/conexao.js')
 
 let app = express();
 
 const PORT = 3001;
 
-app.get('/', (requisicao, resposta) => {
-    resposta.send('Site Trampo Top =)')
+db.authenticate()
+.then (() => {
+    console.log('Conectou ao Banco de Dados')
+})
+.catch((erro) =>{
+    console.log('Erro ao conectar no banco')
+    console.log(erro)
 })
 
+app.use('/categorias', require('./routes/categorias'))
+
 app.listen(PORT, () => {
-    console.log('servidor executando na porta ' + PORT)
+    console.log('Servidor executando na porta ' + PORT)
 })
